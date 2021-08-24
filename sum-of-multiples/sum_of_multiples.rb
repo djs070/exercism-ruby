77e895e31a3d4aa1dee997713560529039a5c13a
David Smith <djs070@gmail.com>
2021-08-24 22:22:59 +1000

    sum_of_multiples: completed solution

---
+++ sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,23 @@
+require 'set'
+
+class SumOfMultiples
+  def initialize(*multiplicands)
+    @multiplicands = multiplicands
+  end
+
+  def to(limit)
+    set = Set.new
+
+    @multiplicands.each do |m|
+      next if m < 1
+
+      multiple = 0
+      while multiple < limit
+        set.add(multiple)
+        multiple += m
+      end
+    end
+
+    set.inject(0, :+)
+  end
+end
